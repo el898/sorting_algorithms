f923b844e39fc48953b7f9b85cc68072a482e15c
el898 <ebubeexcel3@gmail.com>
2023-04-25 15:04:13 +0100

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm
+ * @list: pointer to pointer to head of the list
+ **/
+void insertion_sort_list(listint_t **list)
+{
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	listint_t *curr = (*list)->next;
+	while (curr != NULL)
+	{
+		listint_t *tmp = curr->next;
+
+		while (curr->prev != NULL && curr->n < curr->prev->n)
+		{
+			listint_t *prev = curr->prev;
+
+			prev->next = curr->next;
+			if (curr->next != NULL)
+				curr->next->prev = prev;
+			curr->prev = prev->prev;
+			curr->next = prev;
+			prev->prev = curr;
+			if (curr->prev == NULL)
+				*list = curr;
+			else
+				curr->prev->next = curr;
+
+			print_list(*list);
+		}
+		curr = tmp;
+	}
+}
